94712d61550140f685cd1509493fe535cb0b15a1
Michele Rega <mr.developer@outlook.it>
2024-12-03 21:54:01 +0100

    Python script language tests

---
+++ hello.py
@@ -0,0 +1,14 @@
+# hello.py
+
+def hello():
+  if (3 % 2 == 0):
+    print("Sono in if")
+  else:
+    print("Sono in else")
+
+  for i in range(10):
+    print(i)
+
+  print("Stampa finita")
+
+hello();
